3b66994cb6bbb5bf640ed6bdabce52fb2c72e712
eng-batol <batol.alashwak@gmail.com>
2024-12-11 10:36:13 +0300

    add the trash icon

--- lib/Cart.dart
+++ lib/Cart.dart
@@ -329,6 +329,99 @@
 //     );
 //   }
 // }
+
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:provider/provider.dart';
+// import 'package:webstore/provider.dart';
+
+// class Cart extends StatelessWidget {
+//   const Cart({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor:
+//             Colors.white.withOpacity(0.8), // Set AppBar background color
+//         title: const Text(
+//           "Cart",
+//           style: TextStyle(
+//             color: Colors.black, // Set title color to contrast with background
+//           ),
+//         ),
+//         centerTitle: true, // Center title in AppBar
+//         iconTheme: const IconThemeData(
+//           color: Colors.black, // Set icon color to contrast with background
+//         ),
+//       ),
+//       body: Column(
+//         children: [
+//           const SizedBox(
+//             height: 10,
+//           ),
+//           Expanded(
+//             child: Consumer<CartProvider>(builder: (context, provider, _) {
+//               return ListView.builder(
+//                 itemCount: provider.cart.length,
+//                 itemBuilder: (context, index) {
+//                   return Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: ListTile(
+//                       shape: Border.all(color: Colors.black),
+//                       leading: Text(
+//                         provider.cart[index],
+//                         style: const TextStyle(
+//                           fontSize: 18, // Set the desired font size here
+//                         ),
+//                       ),
+//                     ),
+//                   );
+//                 },
+//               );
+//             }),
+//           ),
+//           const Row(
+//             mainAxisAlignment: MainAxisAlignment.end,
+//             children: [
+//               Padding(
+//                 padding: EdgeInsets.all(8.0),
+//                 child: Text(
+//                   "Total: KWD 50",
+//                   style: TextStyle(fontSize: 25),
+//                 ),
+//               ),
+//             ],
+//           ),
+//           const SizedBox(
+//             height: 30,
+//           ),
+//           ElevatedButton(
+//             onPressed: () {
+//               GoRouter.of(context).push('/checkout');
+//             },
+//             style: ElevatedButton.styleFrom(
+//               backgroundColor:
+//                   Colors.white.withOpacity(0.8), // Set button background color
+//               padding: const EdgeInsets.symmetric(
+//                   horizontal: 10, vertical: 5), // Match button padding
+//             ),
+//             child: const Text(
+//               "Checkout",
+//               style: TextStyle(
+//                 color: Colors.black, // Set text color
+//                 fontSize: 20, // Set font size
+//               ),
+//             ),
+//           ),
+//           const SizedBox(
+//             height: 40,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -373,6 +466,12 @@ class Cart extends StatelessWidget {
                         style: const TextStyle(
                           fontSize: 18, // Set the desired font size here
                         ),
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.black),
+                        onPressed: () {
+                          provider.removeCart(provider.cart[index]);
+                        },
                       ),
                     ),
                   );
